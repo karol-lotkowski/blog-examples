a1afdadd80c7274a57c30e335e894d118812a0d1
Michał Jonko <michal.jonko@gmail.com>
2015-10-06 14:27:01 +0200

    Update WarriorSkills.java
    New instance of Random() was created every time fight() was called.

--- src/main/java/com/karollotkowski/game/WarriorSkills.java
+++ src/main/java/com/karollotkowski/game/WarriorSkills.java
@@ -6,10 +6,11 @@ public class WarriorSkills extends CombatSkills {
 
     private static final int STRENGTH = 10;
     private static final int MANA = 0;
+    private final Random random = new Random();
 
     @Override
     protected int fight() {
-        return new Random().ints(0, STRENGTH).findFirst().getAsInt();
+        return new random.ints(0, STRENGTH).findFirst().getAsInt();
     }
 
     @Override
